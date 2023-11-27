b8d0d1c4366b72f9452fa1bb331341317ef7b270
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-27 14:03:00 +0300

    !

--- src/components/index__main/main-price/main-price.tsx
+++ src/components/index__main/main-price/main-price.tsx
@@ -1,23 +1,40 @@
 import style from './mainPrice.module.scss'
 import Carousel from "nuka-carousel"
+import { Link } from 'react-router-dom';
 
 function MainPrice() {
   return (
     <div className={style.main_price__wrapper}>
       <h2 className={style.main_price__title}>цены</h2>
-      <Carousel autoplay={true}>
-        <div className={style.panel} id="one_panel">
-          <h4 className={style.panel_title}>сайты</h4>
-          <p>Content1</p>
-        </div>
-        <div className={style.panel} id="one_panel">
-          <h4 className={style.panel_title}>сайты</h4>
-          <p>Content2</p>
-        </div>
-        <div className={style.panel} id="one_panel">
-          <h4 className={style.panel_title}>сайты</h4>
-          <p>Content3</p>
-        </div>
+      <Carousel>
+        <Link to='/' className={style.panel}>
+          <div className={style.wrapper}>
+            <img className={style.panel_img}></img>
+            <h4 className={style.panel_title}>сайты</h4>
+            <p>инди</p>
+          </div>
+          <div className={style.wrapper}>
+            <img className={style.panel_img}></img>
+            <h4 className={style.panel_title}>сайты</h4>
+            <p>шаблон</p>
+          </div>
+        </Link>
+        <Link to='/' className={style.panel}>
+          <div className={style.wrapper}>
+            <img className={style.panel_img}></img>
+            <h4 className={style.panel_title}>чат боты</h4>
+            <p>магазины</p>
+            <p>описание</p>
+          </div>
+        </Link>
+        <Link to='/' className={style.panel}>
+          <div className={style.wrapper}>
+            <img className={style.panel_img}></img>
+            <h4 className={style.panel_title}>приложения</h4>
+            <p>Content1</p>
+          </div>
+          
+        </Link>
       </Carousel>
     </div>
   )
