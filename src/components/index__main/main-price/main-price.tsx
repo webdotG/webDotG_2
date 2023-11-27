067f423a88524a5e0a7f4af3279b408b363ea045
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-27 12:27:16 +0300

    fix

--- src/components/index__main/main-price/main-price.tsx
+++ src/components/index__main/main-price/main-price.tsx
@@ -1,26 +1,26 @@
-import './main-price.css'
+import style from './mainPrice.module.scss'
 
 function MainPrice() {
   return (
-    <div className='main-price__wrapper'>
-      <h2 className='main-price__title'>цены</h2>
-      <div className='main-price__wrpaper--green'>
+    <div className={style.main_price__wrapper}>
+      <h2 className={style.main_price__title}>цены</h2>
+      <div className={style.main_price__wrpaper__green}>
 
-        <input className="radio" id="one" name="group" type="radio" defaultChecked={true} />
-        <input className="radio" id="two" name="group" type="radio" />
-        <input className="radio" id="three" name="group" type="radio" />
-        <input className="radio" id="four" name="group" type="radio" />
+        <input className={style.radio} id="one" name="group" type="radio" defaultChecked={true} />
+        <input className={style.radio} id="two" name="group" type="radio" />
+        <input className={style.radio} id="three" name="group" type="radio" />
+        <input className={style.radio} id="four" name="group" type="radio" />
 
-        <div className="tabs">
-          <label className="tab Roboto-Regular" id="one-tab" htmlFor="one"><h3>сайты</h3></label>
-          <label className="tab Roboto-Regular" id="two-tab" htmlFor="two"><h3>приложения</h3></label>
-          <label className="tab Roboto-Regular" id="three-tab" htmlFor="three"><h3>чат боты</h3></label>
-          <label className="tab Roboto-Regular" id="four-tab" htmlFor="four"><h3>email письма</h3></label>
+        <div className={style.tabs}>
+          <label className={style.tab} id="one-tab" htmlFor="one"><h3>сайты</h3></label>
+          <label className={style.tab} id="two-tab" htmlFor="two"><h3>приложения</h3></label>
+          <label className={style.tab} id="three-tab" htmlFor="three"><h3>чат боты</h3></label>
+          <label className={style.tab} id="four-tab" htmlFor="four"><h3>email письма</h3></label>
         </div>
 
-        <div className="panels">
-          <div className="panel" id="one-panel">
-            <div className='main-price__site-description-wrapper'>
+        <div className={style.panels}>
+          <div className={style.panel} id="one-panel">
+            <div className={style.main_price__site_description_wrapper}>
               <h5>лендинг</h5>
               <p>одностраничный сайт-визитка</p>
               <ul>
@@ -28,7 +28,7 @@ function MainPrice() {
                 <li>индивидуальный</li>
               </ul>
             </div>
-            <div className='main-price__site-description-wrapper'>
+            <div className={style.main_price__site_description_wrapper}>
               <h5>корпоротивный сайт</h5>
               <p>одностраничный сайт-визитка</p>
               <ul>
@@ -36,7 +36,7 @@ function MainPrice() {
                 <li>индивидуальный</li>
               </ul>
             </div>
-            <div className='main-price__site-description-wrapper'>
+            <div className={style.main_price__site_description_wrapper}>
               <h5>интернет магазин</h5>
               <p></p>
               <ul>
@@ -45,16 +45,16 @@ function MainPrice() {
               </ul>
             </div>
           </div>
-          <div className="panel" id="two-panel">
-            <h4 className="panel-title">приложения</h4>
+          <div className={style.panel} id="two-panel">
+            <h4 className={style.panel_title}>приложения</h4>
             <p>Content2</p>
           </div>
-          <div className="panel" id="three-panel">
-            <h4 className="panel-title">чат боты</h4>
+          <div className={style.panel} id="three-panel">
+            <h4 className={style.panel_title}>чат боты</h4>
             <p>Content3</p>
           </div>
-          <div className="panel" id="four-panel">
-            <h4 className="panel-title">email письма</h4>
+          <div className={style.panel} id="four-panel">
+            <h4 className={style.panel_title}>email письма</h4>
             <p>Content3</p>
           </div>
         </div>
