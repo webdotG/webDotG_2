87e0ab8c37a8fb1572c8fdbddfa61eb442fcc523
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-28 08:33:12 +0300

    1

--- src/components/index__main/main-portfolio/main-portfolio.tsx
+++ src/components/index__main/main-portfolio/main-portfolio.tsx
@@ -1,45 +1,56 @@
 // import style from './mainPortfolio.module.scss'
-import { useState, useMemo } from 'react' 
 
 
 function MainPortfolio() {
-  const useHover = () => {
-    const [hovered, setHovered] = useState(false);
-
-    const eventHandlers= useMemo(() => ({
-      onMouseOver() { setHovered(true); },
-      onMouseOut() { setHovered(false); }
-    }), []);
-    
-    return [hovered, eventHandlers];
-  }
-  
-  const Item = ({ children } ) => {
-    const [hovered, eventHandlers] = useHover();
-  
-    return (
-      <li {...eventHandlers}>Item: {hovered && children}</li>
-    );
-  };
-  
-  const myObject = {
-    a: 'A1',
-    b: 'B2',
-    c: 'C3',
-  }
-
-  const reactionItems = Object.entries(myObject)
-    .map(([key, value]) => (
-      <Item key={key}>{value}</Item>
-    ));
 
   return (
-    <div className="App">
-      <h2>Reactions</h2>
-      <ul>{reactionItems}</ul>
-    </div>
-  );
-
+    <>
+    
+    </>
+  )
+ 
 }
 
 export default MainPortfolio
+
+
+
+
+// import { useState, useMemo } from 'react' 
+// 
+// const useHover = () => {
+//   const [hovered, setHovered] = useState(false);
+
+//   const eventHandlers= useMemo(() => ({
+//     onMouseOver() { setHovered(true); },
+//     onMouseOut() { setHovered(false); }
+//   }), []);
+  
+//   return [hovered, eventHandlers];
+// }
+
+// const Item = ({ children } ) => {
+//   const [hovered, eventHandlers] = useHover();
+
+//   return (
+//     <li {...eventHandlers}>Item: {hovered && children}</li>
+//   );
+// };
+
+// const myObject = {
+//   a: 'A1',
+//   b: 'B2',
+//   c: 'C3',
+// }
+
+// const reactionItems = Object.entries(myObject)
+//   .map(([key, value]) => (
+//     <Item key={key}>{value}</Item>
+//   ));
+
+// return (
+//   <div className="App">
+//     <h2>Reactions</h2>
+//     <ul>{reactionItems}</ul>
+//   </div>
+// );
