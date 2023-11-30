20ccfcd96db7572014e76ad14f740e23c6501921
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-30 16:13:29 +0300

    motion add

--- src/components/index__main/main-title/main-title.tsx
+++ src/components/index__main/main-title/main-title.tsx
@@ -1,20 +1,37 @@
+import { motion } from 'framer-motion'
 import G from './G';
 import style from './mainTitle.module.scss'
+
+const textAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: custom => ( {
+    x: 0,
+    opacity: 1,
+    transition: {delay: custom * 0.2}
+  })
+}
 
 function MainTitle() {
 
   return (
-    <>
-      <div className={style.main_title__wrapp}>
-        <h1 className={style.main_title}>webDot<G /></h1>
-      </div>
+    <div className={style.mainTitleBlock}>
+      <motion.div
+        initial='hidden'
+        whileInView='visible'
+        className={style.main_title__wrapp}>
+        <motion.h1 variants={textAnimation} className={style.main_title}>webDot<G /></motion.h1>
+        <motion.p custom={2} variants={textAnimation} className={style.main_title_p}>разрабатываю и улучшаю</motion.p>
+      </motion.div>
       <ul className={style.main_title_list}>
-        <li className={style.main_title__text }>сайты</li>
-        <li className={style.main_title__text }>чат боты</li>
-        <li className={style.main_title__text }>приложения</li>
+        <li className={style.main_title__text}>сайты</li>
+        <li className={style.main_title__text}>чат боты</li>
+        <li className={style.main_title__text}>приложения</li>
       </ul>
-    </>
-    )
+    </div>
+  )
 
 }
 
