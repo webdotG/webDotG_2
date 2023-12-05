32aefee0cec18ec0f8beb368fdcec7681ebc6258
Grant Kirill <webdotg.inbox@gmail.com>
2023-12-05 15:59:26 +0300

    input components TS props add

--- src/components/common/fields/inputs/input/input.tsx
+++ src/components/common/fields/inputs/input/input.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './input.scss'
 
-interface typeInputProps extends React.HTMLProps<HTMLInputElement>{}
+interface typeInputProps extends React.HTMLProps<HTMLInputElement> {
+  isError?: boolean;
+  helperText?: string
+}
 
-export const Input: React.FC<typeInputProps> = (props) => {
-  return <input  {...props}/>
+export const Input: React.FC<typeInputProps> = ({ isError = false, helperText, ...props }) => {
+  const className = isError ? 'input_error' : ''
+
+  return (
+    <div className='input_wrapper'>
+      <input className={className}  {...props} />
+      {isError && helperText && <div className='input_helperText'>{helperText}</div> }
+      
+    </div>
+  )
 }
--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,23 +1,35 @@
+import React from 'react'
 import style from './loginPage.module.scss'
 import { Input } from '../../components/common/fields/'
+import { Button } from '../../components/common/buttons'
+
 
 function LoginPage() {
+
+  const [formValue, setFormValue] = React.useState({ username: '', password: '' })
 
   return (
     <div className={style.loginpage_wrapper}>
       <div className={style.login_page_form_wrapper}>
         <section>header form</section>
         <form className={style.form_wrapper}>
-          <div className={style.input_wrapper}>
-            <label htmlFor='input_login'>имя</label>
-            <Input id='input_login' type='text' placeholder='введите имя' />
-            <span>валидация</span>
-          </div>
-          <div className={style.input_wrapper}>
-            <label htmlFor='input_password'>пароль</label>
-            <Input id='input_password' />
-          </div>
-          <button>войти</button>
+          <Input
+          isError={true}
+          helperText='ошибка'
+            value={formValue.username}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setFormValue({ ...formValue, username: e.target.value })}
+            id='input_login'
+            type='text'
+            placeholder='имя' />
+          <Input
+            value={formValue.password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setFormValue({ ...formValue, password: e.target.value })}
+            id='input_password'
+            placeholder='пароль'
+          />
+          <Button>Войти</Button>
         </form>
       </div>
     </div>
